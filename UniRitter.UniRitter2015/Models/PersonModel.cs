ff3e6b3dda33569da16dcb0baa54027ce0db382d
Diego Porto Ritzel <diego@sistemasd.com.br>
2015-06-20 12:12:01 -0300

    Teste via Visual Studio

--- UniRitter.UniRitter2015/Models/PersonModel.cs
+++ UniRitter.UniRitter2015/Models/PersonModel.cs
@@ -10,6 +10,8 @@ namespace UniRitter.UniRitter2015.Models
     {
         public Guid? id { get; set; }
 
+        //todo
+
         [Required]
         [MaxLength(100)]
         public string firstName { get; set; }
