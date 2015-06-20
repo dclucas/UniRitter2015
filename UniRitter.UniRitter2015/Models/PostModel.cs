400be03d2246a3fe05324467fcc63631e935561e
Diego Porto Ritzel <diego@sistemasd.com.br>
2015-06-20 12:03:57 -0300

    Modelos novos

--- UniRitter.UniRitter2015/Models/PostModel.cs
+++ UniRitter.UniRitter2015/Models/PostModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,18 @@ namespace UniRitter.UniRitter2015.Models
 {
     public class PostModel
     {
+        public Guid? id {get; set;};
+
+        [Required]
+        [MaxLength(400)]
+        public string body {get; set;}
+
+        [Required]
+        [MaxLength(100)]
+        public string title {get; set;}
+
+        public PersonModel author {get; set;}
+
+        public string[] tags {get; set;}
     }
 }
