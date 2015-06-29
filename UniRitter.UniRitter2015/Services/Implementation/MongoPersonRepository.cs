3d0313e51293a33ec0d1f4248a892877fdcaaf2d
dclucas <diogo.lucas@gmail.com>
2015-06-28 21:12:42 -0700

    Adding mongo...

---
+++ UniRitter.UniRitter2015/Services/Implementation/MongoPersonRepository.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniRitter.UniRitter2015.Models;
+
+namespace UniRitter.UniRitter2015.Services.Implementation
+{
+    public class MongoPersonRepository : IRepository<PersonModel>
+    {
+        private IMongoDatabase database;
+
+        public MongoPersonRepository()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            database = client.GetDatabase("foo");
+        }
+
+        public PersonModel Add(PersonModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid modelId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PersonModel Update(Guid id, PersonModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PersonModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PersonModel GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
