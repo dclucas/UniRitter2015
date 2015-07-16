96a8dafacf0b7ea7344747d3904ffe6b57506558
dclucas <diogo.lucas@gmail.com>
2015-07-15 21:13:34 -0700

    wip

---
+++ UniRitter.UniRitter2015.SelfHosted/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace UniRitter.UniRitter2015.SelfHosted
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StartApi())
+            {
+                Console.ReadLine(); 
+            }
+        }
+
+        public static IDisposable StartApi()
+        {
+            string baseAddress = "http://localhost:9000/";
+            
+            return WebApp.Start<Startup>(url: baseAddress);
+        }
+    }
+}
--- UniRitter.UniRitter2015.SelfHosted/Startup.cs
+++ UniRitter.UniRitter2015.SelfHosted/Startup.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using Owin;
 using System.Web.Http;
+using Ninject;
+using Ninject.Web.Common.OwinHost;
+using Ninject.Web.WebApi.OwinHost;
+using UniRitter.UniRitter2015.Models;
 using UniRitter.UniRitter2015.SelfHosted.Support;
+using UniRitter.UniRitter2015.Services;
+using UniRitter.UniRitter2015.Services.Implementation;
 
 namespace UniRitter.UniRitter2015.SelfHosted
 {
@@ -11,6 +18,17 @@ namespace UniRitter.UniRitter2015.SelfHosted
         {
             var config = new MyHttpConfiguration();
             app.UseWebApi(config);
+            app.UseNinjectMiddleware(CreateKernel).UseNinjectWebApi(config);
         }
+
+        private static StandardKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            kernel.Bind<IRepository<PersonModel>>().To<MongoPersonRepository>();
+            
+            return kernel;
+        }         
     }
 }
