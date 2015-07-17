1363b18611ce6f12fc78e64e35fff3d22c09b10c
guitutilo <guitutilo_totty@hotmail.com>
2015-07-17 19:47:50 -0300

    Add - CommentModel

--- UniRitter.UniRitter2015/Models/CommentModel.cs
+++ UniRitter.UniRitter2015/Models/CommentModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,16 @@ namespace UniRitter.UniRitter2015.Models
 {
     public class CommentModel
     {
+        public Guid? id { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string body { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string title { get; set; }
+
+        public PersonModel author { get; set; }
     }
 }
