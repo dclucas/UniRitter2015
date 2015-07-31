d5aa1018e615a5ea97cab84e23a8b32d098c9a09
dclucas <diogo.lucas@gmail.com>
2015-07-31 11:49:04 -0700

    Async methods in.

--- UniRitter.UniRitter2015/Startup.cs
+++ UniRitter.UniRitter2015/Startup.cs
@@ -40,7 +40,6 @@ namespace UniRitter.UniRitter2015
 
             kernel.Bind<IApiConfig>().To<ApiConfig>();
             kernel.Bind(typeof(IRepository<>)).To(typeof(MongoRepository<>));
-            //kernel.Bind(typeof(IRepository<>)).To(typeof(InMemoryRepository<>));
         }
     }
 }
