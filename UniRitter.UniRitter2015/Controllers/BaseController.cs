dc45768f75d06dbffe428ea7c79cdd8e3c5396f0
GustavoEmmel <gremmel@gmail.com>
2015-07-31 21:40:21 -0300

    Exercício: refatorar o código de BaseController para retornar dados de forma assíncrona

--- UniRitter.UniRitter2015/Controllers/BaseController.cs
+++ UniRitter.UniRitter2015/Controllers/BaseController.cs
@@ -1,36 +1,32 @@
 ﻿using System;
 using System.Net;
+using System.Threading.Tasks;
 using System.Web.Http;
 using UniRitter.UniRitter2015.Models;
 using UniRitter.UniRitter2015.Services;
 
 namespace UniRitter.UniRitter2015.Controllers
 {
-    abstract public class BaseController<TModel> : ApiController
-        where TModel : class, IModel
+
+    public class BaseController : ApiController
     {
-        public IRepository<TModel> _repo;
+        private readonly IRepository<IModel> _repo;
 
-        public BaseController()
-        {
-
-        }
-
-        public BaseController(IRepository<TModel> repo)
+        public BaseController(IRepository<IModel> repo)
         {
             _repo = repo;
         }
 
         // GET: api/Person
-        public IHttpActionResult Get()
+        public async Task<IHttpActionResult> Get()
         {
-            return Json(_repo.GetAll());
+            return Json(await _repo.GetAll());
         }
 
         // GET: api/Person/5
-        public IHttpActionResult Get(Guid id)
+        public async Task<IHttpActionResult> Get(Guid id)
         {
-            var data = _repo.GetById(id);
+            var data = await _repo.GetById(id);
             if (data != null)
             {
                 return Json(data);
@@ -39,28 +35,29 @@ namespace UniRitter.UniRitter2015.Controllers
         }
 
         // POST: api/Person
-        public IHttpActionResult Post([FromBody] TModel model)
+        public async Task<IHttpActionResult> Post([FromBody] IModel model)
         {
             if (ModelState.IsValid)
             {
-                var data = _repo.Add(model);
+                var data = await _repo.Add(model);
                 return Json(data);
             }
             return BadRequest(ModelState);
         }
 
         // PUT: api/Person/5
-        public IHttpActionResult Put(Guid id, [FromBody] TModel model)
+        public async Task<IHttpActionResult> Put(Guid id, [FromBody] IModel model)
         {
-            var data = _repo.Update(id, model);
+            var data = await _repo.Update(id, model);
             return Json(model);
         }
 
         // DELETE: api/Person/5
-        public IHttpActionResult Delete(Guid id)
+        public async Task<IHttpActionResult> Delete(Guid id)
         {
-            _repo.Delete(id);
+            await _repo.Delete(id);
             return StatusCode(HttpStatusCode.NoContent);
         }
     }
+  
 }
